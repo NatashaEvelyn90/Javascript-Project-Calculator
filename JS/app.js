20be0bb09378a5103551422650cc2e27899a3589
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-05-23 17:26:34 -0500

    pushing code
    updated calculator. Very difficult this time. Will have to double check and re-verify about "try" and "catch" and understand the meaning behind it per research.

--- JS/app.js
+++ JS/app.js
@@ -17,7 +17,38 @@ const equal = document.getElementById("equal");
 equal.addEventListener("click", equalSign);
 
 function equalSign() {
-    
+    try {
+        const results = calculations(input);
+        textScreen.value = results;
+        input = results.toString();
+    } catch {
+        textScreen.value = "ERROR";
+        input = "";
+}
+}
+
+function calculations(expr) {
+    const math = expr.match(/(\d+\.?\d*|\+|\-|\*|\/)/g);
+    if (!math || isNaN(math[0])) throw new Error("Invalid expression");
+
+    let result = parseFloat(math[0]);
+
+    for (let i = 1; i < math.length; i += 2) {
+        const operatorSymbol = math[i];
+        const nextNumber = parseFloat(math[i + 1]);
+
+        if (isNaN(nextNumber)) throw new Error("Invalid number");
+
+        switch (operatorSymbol) {
+            case "+": result += nextNumber; break;
+            case "-": result -= nextNumber; break;
+            case "*": result *= nextNumber; break;
+            case "/": result /= nextNumber; break;
+            default: throw new Error("Invalid operator");
+        }
+    }
+
+    return result;
 }
 
 
@@ -29,4 +60,32 @@ clear.addEventListener("click", clearAll);
 function clearAll() {
 input ="";
 textScreen.value ="";
+}
+
+//! Secret face
+
+const secret = document.getElementById("secret");
+
+const secretScripts = [
+    "I see you have found this button.",
+    "This IS a super secret button ya know.",
+    "Very TOP SECRET",
+    "Were you curious of the use of this button?",
+    "Do you REALLY want to know?",
+    "WELL TOO BAD!",
+    "THE SECRETS STAY WITH ME.",
+    "POOF",
+];
+
+let secretIndex = 0;
+
+secret.addEventListener("click", superSecret);
+function superSecret() {
+    if (secretIndex < secretScripts.length) {
+        textScreen.value = secretScripts[secretIndex];
+    }else {
+        textScreen.value = "";
+        secret.style.display = "none";
+    }
+    secretIndex++;
 }
