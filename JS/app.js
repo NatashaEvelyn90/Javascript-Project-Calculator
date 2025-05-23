f93560b58cb0d305b7e7799d08aa77b57ebe02b0
Natasha M. Evelyn <natasha.evelyn@mscoding.org>
2025-05-23 11:50:44 -0500

    pushing code
    buttons can click. No equal function yet

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,32 @@
-let numbers = document.querySelectorAll(".calcButtons");
-let equal = document.querySelector(".equal");
-let clear = document.querySelector(".clear");
+//! To type the keys
+const buttons = document.querySelectorAll(".calcButtons");
+const textScreen = document.getElementById("textScreen");
+let input ='';
 
+buttons.forEach((nButton) => {
+    nButton.addEventListener("click",inputNumbers);
+
+    function inputNumbers() {
+          input += nButton.innerText;
+          textScreen.value = input;
+    };
+});
+
+//! To equal equations
+const equal = document.getElementById("equal");
+equal.addEventListener("click", equalSign);
+
+function equalSign() {
+    
+}
+
+
+
+//! To clear the calculator
+const clear = document.getElementById("clear");
+clear.addEventListener("click", clearAll);
+
+function clearAll() {
+input ="";
+textScreen.value ="";
+}
